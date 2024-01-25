f3bf855e977344580649af9610df3c183a4c71dc
Leon van Zyl <leon.vanzyl@gmail.com>
2024-01-25 14:47:06 +0200

    lesson-5

---
+++ conversation-retrieval.py
@@ -0,0 +1,100 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.chains import create_retrieval_chain
+
+# Conversation imports
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+
+def get_documents_from_web(url):
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400,
+        chunk_overlap=20
+    )
+    splitDocs = splitter.split_documents(docs)
+    return splitDocs
+
+def create_db(docs):
+    embedding = OpenAIEmbeddings()
+    vectorStore = FAISS.from_documents(docs, embedding=embedding)
+    return vectorStore
+
+def create_chain(vectorStore):
+    model = ChatOpenAI(
+        model="gpt-3.5-turbo-1106",
+        temperature=0.4
+    )
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's questions based on the context: {context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}")
+    ])
+
+    # chain = prompt | model
+    chain = create_stuff_documents_chain(
+        llm=model,
+        prompt=prompt
+    )
+
+    # Replace retriever with history aware retriever
+    retriever = vectorStore.as_retriever(search_kwargs={"k": 3})
+
+    retriever_prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    history_aware_retriever = create_history_aware_retriever(
+        llm=model,
+        retriever=retriever,
+        prompt=retriever_prompt
+    )
+
+    retrieval_chain = create_retrieval_chain(
+        # retriever, Replace with History Aware Retriever
+        history_aware_retriever,
+        chain
+    )
+
+    return retrieval_chain
+
+
+def process_chat(chain, question, chat_history):
+    response = chain.invoke({
+        "chat_history": chat_history,
+        "input": question,
+    })
+    return response["answer"]
+
+
+
+
+if __name__ == "__main__":
+    docs = get_documents_from_web('https://python.langchain.com/docs/expression_language/')
+    vectorStore = create_db(docs)
+    chain = create_chain(vectorStore)
+
+    # Initialize chat history
+    chat_history = []
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            break
+        response = process_chat(chain, user_input, chat_history)
+        chat_history.append(HumanMessage(content=user_input))
+        chat_history.append(AIMessage(content=response))
+        print("Assistant:", response)
--- retrieval-chain.py
+++ retrieval-chain.py
@@ -10,57 +10,55 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains import create_retrieval_chain
 
-# Retrieve Data
-def get_docs():
-    loader = WebBaseLoader('https://python.langchain.com/docs/expression_language/')
+def get_documents_from_web(url):
+    loader = WebBaseLoader(url)
     docs = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(
+    
+    splitter = RecursiveCharacterTextSplitter(
         chunk_size=200,
         chunk_overlap=20
     )
-
-    splitDocs = text_splitter.split_documents(docs)
-
+    splitDocs = splitter.split_documents(docs)
     return splitDocs
 
-def create_vector_store(docs):
+def create_db(docs):
     embedding = OpenAIEmbeddings()
     vectorStore = FAISS.from_documents(docs, embedding=embedding)
     return vectorStore
 
-
 def create_chain(vectorStore):
     model = ChatOpenAI(
-        temperature=0.4,
-        model='gpt-3.5-turbo-1106'
+        model="gpt-3.5-turbo-1106",
+        temperature=0.4
     )
 
     prompt = ChatPromptTemplate.from_template("""
-    Answer the user's question.
+    Answer the user's question:
     Context: {context}
-    Question: {input}
+    Question: {input}                                       
     """)
 
     # chain = prompt | model
-    document_chain = create_stuff_documents_chain(
+    chain = create_stuff_documents_chain(
         llm=model,
         prompt=prompt
     )
 
-    retriever = vectorStore.as_retriever()
+    retriever = vectorStore.as_retriever(search_kwargs={"k": 1})
 
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    retrieval_chain = create_retrieval_chain(
+        retriever,
+        chain
+    )
 
     return retrieval_chain
 
 
-docs = get_docs()
-vectorStore = create_vector_store(docs)
+docs = get_documents_from_web('https://python.langchain.com/docs/expression_language/')
+vectorStore = create_db(docs)
 chain = create_chain(vectorStore)
 
 response = chain.invoke({
     "input": "What is LCEL?",
 })
-
-print(response)
+print(response["context"])
